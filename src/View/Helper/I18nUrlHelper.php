04ffc0ea7125edefa8a86db6863a4a08f1853ac2
Erwane Breton <erwane@phea.fr>
2018-05-24 21:28:34 +0200

    rename url helper

--- src/View/Helper/I18nUrlHelper.php
+++ src/View/Helper/I18nUrlHelper.php
@@ -4,10 +4,15 @@ namespace I18nUrl\View\Helper;
 use Cake\View\Helper;
 use I18nUrl\I18n\LocaleMiddleware;
 
-class I18nRouteHelper extends Helper
+class I18nUrlHelper extends Helper
 {
     public function byName($name)
     {
         return ['_name' => $name . '.' . LocaleMiddleware::getLocale(true)];
+    }
+
+    public function getLocale($clean = false)
+    {
+        return LocaleMiddleware::getLocale($clean);
     }
 }
