d97f12f9a9ff9408a23d493c5ed9fb5a4692483e
Erwane Breton <erwane@phea.fr>
2018-05-25 09:35:11 +0200

    url helper link() & build()

--- src/View/Helper/I18nUrlHelper.php
+++ src/View/Helper/I18nUrlHelper.php
@@ -6,6 +6,8 @@ use I18nUrl\I18n\LocaleMiddleware;
 
 class I18nUrlHelper extends Helper
 {
+    public $helpers = ['Html', 'Url'];
+
     public function byName($name)
     {
         return ['_name' => $name . '.' . $this->getLocale(true)];
@@ -14,5 +16,25 @@ class I18nUrlHelper extends Helper
     public function getLocale($clean = false)
     {
         return LocaleMiddleware::getLocale($clean);
+    }
+
+    public function link($title, $url = null, array $options = [])
+    {
+        if (is_array($url) && isset($url['_name'])) {
+            $localized = $this->byName($url['_name']);
+            $url['_name'] = $localized['_name'];
+        }
+
+        return $this->Html->link($title, $url, $options);
+    }
+
+    public function build($url = null, $options = false)
+    {
+        if (is_array($url) && isset($url['_name'])) {
+            $localized = $this->byName($url['_name']);
+            $url['_name'] = $localized['_name'];
+        }
+
+        return $this->Url->build($url, $options);
     }
 }
