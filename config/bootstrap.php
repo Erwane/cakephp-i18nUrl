451736b5212cae764f7e3bb84bbc10c5bc13b1e6
Erwane Breton <erwane@phea.fr>
2019-10-04 23:26:31 +0200

    set I18n.languages and default configurations

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,8 +1,16 @@
 <?php
 
+use Cake\Core\Configure;
 use Cake\Event\EventManager;
 
 EventManager::instance()->on('Controller.beforeRedirect',
     ['priority' => 999],
     ['I18nUrl\Controller\Controller', 'beforeRedirect']
 );
+
+if (!Configure::read('I18n.languages')) {
+    Configure::write('I18n.languages', ['en']);
+}
+if (!Configure::read('I18n.default')) {
+    Configure::write('I18n.default', 'en');
+}
